ad6345ff5369ac45d4512654841e6fb99c9b3de5
Gerardo <gerstev.32@gmail.com>
2025-07-09 11:18:37 -0600

    agregando render

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import clientsRoutes from "./src/routes/clienteRoutes.js"
 const app = express()
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://evaluacion-20-8wz2.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
